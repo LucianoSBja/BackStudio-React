68da7dcb6b0e9ac487de74ef5c3b14531788425d
LucianoSBja <lucianosotobonja95@gmail.com>
2023-06-19 23:23:22 -0300

    agregando header

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,4 +1,13 @@
 const Header = () => {
-	return <div>Header</div>
+	return (
+		<header className="main__header">
+			<div className="container main__header-container">
+				<div className="main__header-left">
+					<h4>#100DaysOfWorkOut</h4>
+				</div>
+				<div className="main__header-right"></div>
+			</div>
+		</header>
+	)
 }
 export default Header
