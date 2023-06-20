0c1e3ed2a7eab5ad199e7716104cffe58314412e
LucianoSBja <lucianosotobonja95@gmail.com>
2023-06-19 23:03:01 -0300

    solucionando links

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,14 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Navbar />
-			<Home />
-			<About />
-			<Gallery />
-			<Plans />
-			<Contact />
-			<NotFound />
+			<Routes>
+				<Route index element={<Home />} />
+				<Route path="about" element={<About />} />
+				<Route path="contact" element={<Contact />} />
+				<Route path="gallery" element={<Gallery />} />
+				<Route path="plans" element={<Plans />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
 		</BrowserRouter>
 	)
 }
