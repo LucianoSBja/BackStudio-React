cba3d0ab746cf184e82070f1c532e104e852da34
LucianoSBja <lucianosotobonja95@gmail.com>
2023-06-27 00:00:19 -0300

    testimoniales

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import MainHeader from '../../components/MainHeader'
+import Testimonials from '../../components/Testimonials'
 import './home.css'
 
 const Home = () => {
 	return (
 		<>
 			<MainHeader />
+			<Testimonials />
 		</>
 	)
 }
